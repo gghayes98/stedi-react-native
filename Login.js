7868d359d96f7763b26030c41b979a4f94f43da0
gghayes98 <gghayes98@gmail.com>
2022-06-16 15:00:42 -0600

    Fixed the bug not letting it run and then set up OTP

--- Login.js
+++ Login.js
@@ -1,5 +1,15 @@
 import {useState} from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Text, TouchableOpacity} from "react-native";
+
+const sendText = async (phoneNumber) => {
+  console.log("Phone Number: ", phoneNumber);
+  await fetch('https://dev.stedi.me/twofactorlogin/' + phoneNumber, {
+    method: 'POST',
+    headers:{
+      'content-type':'application/text'
+    }
+  })
+}
 
 const Login = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -12,15 +22,29 @@ const Login = () => {
         onChangeText={setPhoneNumber}
         value={phoneNumber}
         placeholder="801-867-5309"
+        placeholderTextColor='#D3D3D3'
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{sendText(phoneNumber)}}
+      >
+        <Text>Send OTP</Text>
+      </TouchableOpacity>
       <TextInput
         style={styles.input}
         onChangeText={setOneTimePassword}
         value={oneTimePassword}
         placeholder="1234"
+        placeholderTextColor='#D3D3D3'
         keyboardType="numeric"
         secureTextEntry = {true}
       />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=>{console.log('Login button was clicked')}}
+      >
+        <Text>Login</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -34,7 +58,12 @@ const styles = StyleSheet.create({
   },
   margin:{
     marginTop:100
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
   }
 });
 
-export default UselessTextInput;
+export default Login;
